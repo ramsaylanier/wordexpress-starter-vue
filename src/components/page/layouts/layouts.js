30052731d7155107f2e1ae5bb607cf84daebd992
Ramsay Lanier <ramsaylanier@gmail.com>
2018-01-02 16:20:20 -0500

    page with sidebar component

--- src/components/page/layouts/layouts.js
+++ src/components/page/layouts/layouts.js
@@ -1,9 +1,11 @@
 import DefaultPage from './DefaultPage'
 import PageWithHeader from './PageWithHeader'
+import PageWithSidebar from './PageWithSidebar'
 import PostList from '@/components/post/PostList'
 
 export default {
   DefaultPage,
   PageWithHeader,
+  PageWithSidebar,
   PostList
 }
