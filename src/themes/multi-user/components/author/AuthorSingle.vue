35047add44a1bf8f8103c5572fbc1aff14327bf8
Ramsay Lanier <rlanier@ALT8AG8WL.local>
2018-01-11 14:52:38 -0500

    add multi-user theme

---
+++ src/themes/multi-user/components/author/AuthorSingle.vue
@@ -0,0 +1,88 @@
+<template>
+  <author-container class="author-page">
+    <div class="author-content" slot-scope="data" v-if="data.author">
+      <page-header>
+        <span slot="title">{{data.author.display_name}}</span>
+      </page-header>
+
+      <div class="body">
+        <div class="wrapper">
+          
+          <div class="avatar">
+            <img :src="$getAvatarLink(data.author.user_email, 150)" :alt="`${data.author.display_name} Avatar`"/>
+          </div>
+
+          <transition
+            appear
+            @enter="enter"
+            :css="false"
+          >
+            <div class="posts" v-if="!data.isLoading">
+              <post-list-item v-for="post in data.author.posts" :key="post.id" :post="post" ref="posts"/>
+            </div>
+          </transition>
+        </div>
+      </div>
+    </div>
+  </author-container>
+</template>
+
+<script>
+import AuthorContainer from './containers/AuthorContainer'
+import PageHeader from '@/components/page/PageHeader'
+import PostListItem from '@/components/post/PostListItem'
+import {TweenMax, Power4} from 'gsap'
+export default {
+  name: 'author-single',
+  components: {
+    AuthorContainer, PageHeader, PostListItem
+  },
+  methods: {
+    enter: function (el, done) {
+      TweenMax.staggerFromTo(el.children, 2, {
+        y: 20,
+        alpha: 0
+      }, {
+        y: 0,
+        alpha: 1,
+        ease: Power4.easeOut,
+        onComplete: done
+      }, 0.1)
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+  @import '../../styles/colors.scss';
+  .author-header{
+    display: flex;
+    justify-content: center;
+    padding: 4rem 0;
+  }
+    
+  .wrapper{
+    position: relative;
+    max-width: 800px;
+    margin: 0 auto;
+  }
+
+  .avatar{
+    position: absolute;
+    top: -75px;
+    right: 0;
+    z-index: 2;
+    img{
+      border-radius: 50%;
+      border: 5px solid $dark-color;
+      box-sizing: content-box;
+    }
+  }
+
+  .posts{
+    position: relative;
+    display: grid;
+    grid-row-gap: 1rem;
+    padding-top: 2rem;
+    z-index: 1;
+  }
+</style>
---
+++ src/themes/multi-user/components/author/containers/AuthorContainer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <slot :author="user" :isLoading="isLoading"></slot>
+  </div>
+</template>
+
+<script>
+import UserQuery from 'core/graphql/user.gql'
+import {willPrefetch} from 'vue-apollo'
+
+export default willPrefetch({
+  name: 'author-container',
+  data () {
+    return {
+      isLoading: 0
+    }
+  },
+  apollo: {
+    user: {
+      query: UserQuery,
+      loadingKey: 'isLoading',
+      prefetch ({route}) {
+        return {
+          name: route.params.name
+        }
+      },
+      variables () {
+        return {
+          name: this.$route.params.name
+        }
+      }
+    }
+  }
+})
+</script>
