f88bb2f01a08bf95e9f20bdc2444095de4d5a52c
Ramsay Lanier <ramsaylanier@gmail.com>
2018-01-11 14:52:09 -0500

    add apollo link error

--- src/core/apollo.js
+++ src/core/apollo.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
+import { onError } from 'apollo-link-error'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 import config from 'config'
@@ -29,9 +30,22 @@ export function createApolloClient (ssr = false) {
     fetch
   })
 
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.map(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+        ),
+      )
+    }
+
+    if (networkError) console.log(`[Network error]: ${networkError}`)
+  })
+
   // Create the apollo client
   const apolloClient = new ApolloClient({
     link,
+    errorLink,
     cache: new InMemoryCache(),
     connectToDevTools: true,
     ...(ssr ? {
